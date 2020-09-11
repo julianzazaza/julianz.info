e5d98c1cb858a573a9cccded11960fb0f7fffef4
Julian <julianzabala1@gmail.com>
2020-09-10 17:28:04 -0700

    Updates

---
+++ precache-manifest.ce288ae97b28797ec0ff897a3b810f82.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4df584e491c80bdf68dd27804999ff6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bd2539e1806736ad7946",
+    "url": "/static/css/main.4b3fba68.chunk.css"
+  },
+  {
+    "revision": "2cf022770dc0c6128e70",
+    "url": "/static/js/2.f63f47a3.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.f63f47a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd2539e1806736ad7946",
+    "url": "/static/js/main.f507ef25.chunk.js"
+  },
+  {
+    "revision": "2703770beeea2c1705d7",
+    "url": "/static/js/runtime-main.78a3e385.js"
+  },
+  {
+    "revision": "21c5c0d9932af2d48f69c1b007b4e76c",
+    "url": "/static/media/Apercu.21c5c0d9.woff2"
+  },
+  {
+    "revision": "52448f2de93ac4a043a938c29e38ce19",
+    "url": "/static/media/Apercu.52448f2d.woff"
+  },
+  {
+    "revision": "5db8bbfb6a548babd3c88584bc301dd2",
+    "url": "/static/media/Bemio.5db8bbfb.otf"
+  },
+  {
+    "revision": "2927903d9b361c5e41921c28c0bc0487",
+    "url": "/static/media/logo.2927903d.png"
+  }
+]);
