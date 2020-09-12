9fe33bfc6df1cdbd6503da059532c2f8c743eaeb
Julian <julianzabala1@gmail.com>
2020-09-12 04:43:39 -0700

    Styling for portfolio

--- src/components/Art/index.style.js
+++ src/components/Art/index.style.js
@@ -2,6 +2,11 @@ import styled from "styled-components";
 
 export const ArtWrapper = styled.div`
   text-align: center;
+  color: #f49c77;
   max-width: 1080px;
   margin: auto;
+  button {
+    color: #f49c77;
+    font-family: Apercu;
+  }
 `;
