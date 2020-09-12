68006bd3790e29e5e462867d97599887bdb218d0
Julian <julianzabala1@gmail.com>
2020-09-12 02:47:04 -0700

    Use helmet for setting title/description/logo

--- src/components/Art/index.js
+++ src/components/Art/index.js
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 import Navigation from "./Navigation";
 import PhotoSet from "./PhotoSet";
 import { ArtWrapper } from "./index.style";
+import { Helmet } from "react-helmet";
+import logo from "./logo.png";
 
 const Art = () => {
   const [photoBody, setPhotoBody] = useState("base");
@@ -10,6 +12,11 @@ const Art = () => {
   };
   return (
     <ArtWrapper>
+      <Helmet>
+        <title>A Visual Diary</title>
+        <meta name="description" content="Julian's Public Diary" />
+        <link rel="icon" type="image/png" href={logo} sizes="16x16" />
+      </Helmet>
       <Navigation onBodyChange={onBodyChange} />
       <PhotoSet typeName={photoBody} />
     </ArtWrapper>
