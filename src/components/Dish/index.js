59c3a3ac7068c3d0c103f9d9bb380049f81fdade
Julian <julianzabala1@gmail.com>
2020-09-12 02:47:42 -0700

    Use helmet for rendering dish-specific head components

--- src/components/Dish/index.js
+++ src/components/Dish/index.js
@@ -3,10 +3,20 @@ import Header from "./Header";
 import MovieList from "./MovieList";
 import Footer from "./Footer";
 import { DishWrapper } from "./index.style";
+import { Helmet } from "react-helmet";
+import favicon from "./dishcord.ico";
 const Dish = () => {
   document.title = "Dishcord Movie Nights";
   return (
     <DishWrapper>
+      <Helmet>
+        <title>Dishcord Movie Nights</title>
+        <meta
+          name="description"
+          content="Fun lil' web app to keep track of moves we watch."
+        />
+        <link rel="icon" type="image/png" href={favicon} sizes="16x16" />
+      </Helmet>
       <Header />
       <MovieList />
       <Footer />
