581e97a50fcaf95a7719d5f6eea949b83917f649
Julian <julianzabala1@gmail.com>
2025-02-16 22:47:01 -0800

    Fix body styling

---
+++ src/components/MainWindow/Title/index.style.ts
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  text-align: center;
+  background-color: #c0c0c0;
+  background: repeating-linear-gradient(0deg, black, #c0c0c0 1px, #c0c0c0 1px, #c0c0c0 5px);
+  border: 2px solid #cecefb;
+  outline: 2px solid black;
+  text-align: center;
+  height: 28px;
+`;
+export const StyledTitle = styled.span`
+  background-color: #c0c0c0;
+  display: inline-block;
+  font-weight: 300;
+  font-style: bold;
+  font-size: 24px;
+  padding: 0 4px 0 4px;
+`;
---
+++ src/components/MainWindow/index.style.ts
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  min-width: 300px;
+  max-width: 700px;
+  margin: 24px auto;
+  background-color: #ffffff;
+  border: 1px solid #cecefb;
+  outline: 1px solid black;
+  border-bottom: 2px solid black;
+  border-right: 2px solid black;
+`;
+
+export default Wrapper;
