3703b07e5a02b26724072f1f2ea2eeea985a2edd
Julian <julianzabala1@gmail.com>
2025-02-19 23:02:26 -0800

    Add new font thats more legible in paragraphs

--- src/fonts/fonts.js
+++ src/fonts/fonts.js
@@ -1,11 +1,18 @@
-import { createGlobalStyle } from "styled-components";
-import sysfont from "./sysfont.otf";
-
+import { createGlobalStyle } from 'styled-components';
+import sysfont from './sysfont.otf';
+import AmericanTypewriter from './AmericanTypewriter.otf';
 export default createGlobalStyle`
 @font-face {
     font-family: 'sysfont';
     src: local('sysfont'),
     url(${sysfont}) format("opentype");
+    font-weight: normal;
+    font-style: normal;
+}
+@font-face {
+    font-family: 'AmericanTypewriter';
+    src: local('AmericanTypewriter'),
+    url(${AmericanTypewriter}) format("opentype");
     font-weight: normal;
     font-style: normal;
 }
